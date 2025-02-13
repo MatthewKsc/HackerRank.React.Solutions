8dda481aea6b270002e0366832038d728ee0a0a6
MatthewKsc <55882615+MatthewKsc@users.noreply.github.com>
2025-02-13 21:52:30 +0100

    Add Item List Manager answear code

--- item-list-manager/src/App.js
+++ item-list-manager/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+// import "h8k-components";
+
+import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [input, setInput] = useState("");
+
+  const handleAddItem = () => {
+    if (input !== '') {
+      setItems([...items, input]);
+      setInput("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <h8k-navbar header="Item List Manager"></h8k-navbar> */} 
+      <div className="App">
+        <h3>Item List</h3>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Enter item"
+          data-testid="input-field"
+        />
+        <button onClick={handleAddItem} data-testid="add-button">
+          Add Item
+        </button>
+        <ul data-testid="item-list">
+          {items.map((item, index) => (
+            <li key={index} data-testid="list-item">
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
